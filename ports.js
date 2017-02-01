b454205b736cd6aa0dfc521001959480ac8ccd34
Rémy HUBSCHER <rhubscher@mozilla.com>
2017-02-01 11:27:47 +0100

    Revert "Revert "Switch back to storage.local in master""
    This reverts commit 026ade441d166dbdd5079cbe95fc70ca8e30a0ba.

--- ports.js
+++ ports.js
@@ -55,7 +55,7 @@ app.ports.getData.subscribe(function(token) {
   if (typeof chrome == "undefined" || typeof chrome.storage == "undefined") {
     decryptAndNotify(passphrase, localStorage.getItem(key));
   } else {
-    chrome.storage.sync.get(
+    chrome.storage.local.get(
       key,
       data => {
         if (chrome.runtime.lastError) {
@@ -97,7 +97,7 @@ app.ports.setData.subscribe(function(content) {
         var data = {};
         data[key] = encryptedContent;
         console.log('set', data)
-        chrome.storage.sync.set(
+        chrome.storage.local.set(
           data,
           () => {
             if (chrome.runtime.lastError) {
