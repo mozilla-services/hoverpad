4623ddec3f80c6c19ec420e51e609f65ffbb7d68
vladikoff <vlad.filippov@gmail.com>
2017-05-11 00:38:03 -0400

    fxa-local prototype

--- background.js
+++ background.js
@@ -1,5 +1,96 @@
+const REDIRECT_URL = browser.identity.getRedirectURL();
+const CLIENT_ID = '3a7d05df0d8db810';
+const SCOPES = ['profile keys'];
+const AUTH_URL =
+`http://127.0.0.1:9010/v1/authorization
+?client_id=${CLIENT_ID}
+&state=state
+&redirect_uri=${encodeURIComponent(REDIRECT_URL)}
+&scope=${encodeURIComponent(SCOPES.join(' '))}`;
+const TOKEN_URL = `http://127.0.0.1:9010/v1/token`;
+const KEYS_URL = `http://127.0.0.1:9010/v1/keys`;
+
+// TODO: move to server
+const CLIENT_SECRET = 'aaf992e3ccf3a8932c8c1aa4e7cce475a6059c73656cda0c9d91873d19a84f31';
+
 const authenticateURL = "https://kinto.dev.mozaws.net/v1/fxa-oauth/login?redirect=https://natim.github.io/get_fxa_token/%23";
 const redirectIntermediate = "https://natim.github.io/get_fxa_token/#";
+
+
+function createKeyPair () {
+  return window.crypto.subtle.generateKey(
+    {
+      name: "RSA-OAEP",
+      modulusLength: 4096,
+      publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
+      hash: {name: "SHA-256"},
+    },
+    true, // extractable key
+    ["encrypt", "decrypt"]
+  ).then(function(keys) {
+    let exportPrivateKey;
+    return window.crypto.subtle.exportKey("jwk", keys.privateKey)
+      .then(function (pk) {
+        exportPrivateKey = pk;
+        return window.crypto.subtle.exportKey("jwk", keys.publicKey);
+      })
+      .then(function (exportPublicKey) {
+        return {
+          keys: keys,
+          exportPrivateKey: exportPrivateKey,
+          exportPublicKey: exportPublicKey
+        };
+      });
+  });
+}
+
+function extractAccessToken(redirectUri) {
+  let m = redirectUri.match(/[#\?](.*)/);
+  if (!m || m.length < 1)
+    return null;
+  let params = new URLSearchParams(m[1].split('#')[0]);
+  return params.get('code');
+}
+
+function getBearerToken(code) {
+  var myHeaders = new Headers();
+  myHeaders.append('Content-Type', 'application/json');
+
+  return fetch(new Request(TOKEN_URL, {
+    method: 'POST',
+    headers: myHeaders,
+    body: JSON.stringify({
+      code: code,
+      client_id: CLIENT_ID,
+      client_secret: CLIENT_SECRET
+    })
+  }))
+  .then(function(response) {
+    if(response.status == 200) return response.json();
+    else throw new Error('Something went wrong on api server!');
+  })
+  .catch(function(error) {
+    console.error(error);
+  });
+}
+
+function getDerivedKeys(bearerToken) {
+  var myHeaders = new Headers();
+  myHeaders.append('Authorization', 'Bearer ' + bearerToken.access_token);
+
+  return fetch(new Request(KEYS_URL, {
+    method: 'POST',
+    headers: myHeaders
+  }))
+  .then(function(response) {
+    if(response.status == 200) return response.json();
+    else throw new Error('Something went wrong on api server!');
+  })
+  .catch(function(error) {
+    console.error(error);
+  });
+}
+
 
 function tabCallback(tabId, changeInfo, updatedTab) {
   if (changeInfo.status === 'complete' && updatedTab.url.indexOf(redirectIntermediate) === 0) {
@@ -54,8 +145,47 @@ function handlePassphraseCleaning() {
 }
 
 function handleAuthentication() {
-  chrome.tabs.create({ 'url': authenticateURL }, function () {
-    chrome.tabs.onUpdated.addListener(tabCallback);
+  // chrome.tabs.create({ 'url': authenticateURL }, function () {
+  //   chrome.tabs.onUpdated.addListener(tabCallback);
+  // });
+  return createKeyPair()
+    .then(function (keyMaterial) {
+      const publicKey = JSON.stringify(keyMaterial.exportPublicKey);
+      return browser.identity.launchWebAuthFlow({
+        interactive: true,
+        url: `${AUTH_URL}&jwk=${publicKey}`
+      });
+  }).then(function (redirectURL) {
+    const code = extractAccessToken(redirectURL);
+
+    return getBearerToken(code);
+  }).then(function (bearer) {
+    console.log('bearer', bearer);
+
+    return getDerivedKeys(bearer).then(function (keys) {
+      return {
+        bearer: bearer,
+        keys: keys
+      };
+    });
+  }).then(function (creds) {
+    const bearer = creds.bearer;
+    const keys = creds.keys;
+    console.log('creds', creds);
+    console.log('keys', keys);
+
+    chrome.storage.local.set({bearer: bearer, keys: keys}, function() {
+      if (chrome.runtime.lastError) {
+        console.error(chrome.runtime.lastError);
+        chrome.runtime.sendMessage({ action: 'error', error: chrome.runtime.lastError });
+      } else {
+        chrome.runtime.sendMessage({ action: 'authenticated', bearer: bearer, keys: keys });
+      }
+    });
+  })
+  .catch(function (err) {
+    console.error(err);
+    throw err;
   });
 }
 
